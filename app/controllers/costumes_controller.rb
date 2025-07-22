b509d49489f9bd9f515325f193f62590bf769623
Mizuki Harthan <mizharthan@gmail.com>
2025-07-22 22:20:39 +0900

    moved file location

---
+++ app/controllers/costumes_controller.rb
@@ -0,0 +1,24 @@
+class CostumesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    @costume.user = current_user
+
+    if @costume.save
+      redirect_to @costume, notice: "Costume was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:costume_size, :name, :description, :price_per_day, :image)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_19_080116) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_22_120137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+    t.integer "size"
+    t.string "description"
+    t.integer "price_per_day"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.index ["user_id"], name: "index_costumes_on_user_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "costume_id", null: false
+    t.bigint "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["costume_id"], name: "index_rentals_on_costume_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +47,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_080116) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "costumes", "users"
+  add_foreign_key "rentals", "costumes"
+  add_foreign_key "rentals", "users"
 end
