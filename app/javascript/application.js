b47888b632f48003a23adcf6b073698d52ab6f6b
reyna chu <reynachu13@gmail.com>
2025-08-02 11:04:37 +0900

    date picker working and looks better

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,10 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import flatpickr from "flatpickr";
+
+document.addEventListener("turbo:load", () => {
+  console.log("Initializing flatpickr");
+  flatpickr("#start-date", { dateFormat: "Y-m-d" });
+  flatpickr("#end-date", { dateFormat: "Y-m-d" });
+});
